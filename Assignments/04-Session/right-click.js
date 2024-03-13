89e83e8bd0aa476fc42d05e7a329c49a1597b0b9
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2024-03-13 06:05:00 +0530

    add alert

--- Assignments/04-Session/right-click.js
+++ Assignments/04-Session/right-click.js
@@ -6,10 +6,11 @@ document.addEventListener('keydown', function(event) {
     if ((event.ctrlKey || event.metaKey) && event.key === 'u') {
         // var closeSite = confirm("Are you sure you want to close this site?");
         event.preventDefault();
-        var closeSite = true;
-        if (closeSite) {
-            window.close();
-        }
+        alert('Viewing page source is disabled.'); 
+        // var closeSite = true;
+        // if (closeSite) {
+        //     window.close();
+        // }
     }
 });
 
