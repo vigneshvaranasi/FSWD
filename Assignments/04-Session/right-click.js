053df952957d57e60fa0d2cc03203656a428bc10
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2024-03-13 05:47:22 +0530

    Add tab closing feature

--- Assignments/04-Session/right-click.js
+++ Assignments/04-Session/right-click.js
@@ -1,3 +1,13 @@
 document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
 });
+
+document.addEventListener('keydown', function(event) {
+    if ((event.ctrlKey || event.metaKey) && event.key === 'u') {
+        // var closeSite = confirm("Are you sure you want to close this site?");
+        var closeSite = true;
+        if (closeSite) {
+            window.close();
+        }
+    }
+});
