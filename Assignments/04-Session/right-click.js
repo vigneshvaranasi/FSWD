401661e32fee52b23fa37110fd2ad8eeb5efb34a
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2024-03-13 05:54:50 +0530

    Re fix the tab closing issue

--- Assignments/04-Session/right-click.js
+++ Assignments/04-Session/right-click.js
@@ -5,9 +5,18 @@ document.addEventListener('contextmenu', function(event) {
 document.addEventListener('keydown', function(event) {
     if ((event.ctrlKey || event.metaKey) && event.key === 'u') {
         // var closeSite = confirm("Are you sure you want to close this site?");
+        event.preventDefault();
         var closeSite = true;
         if (closeSite) {
             window.close();
         }
     }
 });
+
+/* document.addEventListener('keydown', function(event) {
+    if ((event.ctrlKey || event.metaKey) && event.key === 'u') {
+        event.preventDefault();
+        // alert('Viewing page source is disabled.'); 
+    }
+});
+ */
