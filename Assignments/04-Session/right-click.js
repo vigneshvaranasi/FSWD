de59c1cc13c60a1e990db3d2ae021b43981686b0
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2024-03-13 06:11:27 +0530

    fix viewing inspect tools

--- Assignments/04-Session/right-click.js
+++ Assignments/04-Session/right-click.js
@@ -2,7 +2,7 @@ document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
 });
 
-document.addEventListener('keydown', function(event) {
+/* document.addEventListener('keydown', function(event) {
     if ((event.ctrlKey || event.metaKey) && event.key === 'u') {
         // var closeSite = confirm("Are you sure you want to close this site?");
         event.preventDefault();
@@ -12,7 +12,15 @@ document.addEventListener('keydown', function(event) {
         //     window.close();
         // }
     }
+}); */
+
+document.addEventListener('keydown', function(event) {
+    if ((event.ctrlKey || event.metaKey) && (event.key === 'u' || (event.shiftKey && event.code === 'KeyI'))) {
+        event.preventDefault();
+        alert('Viewing page source is disabled.');
+    }
 });
+
 
 /* document.addEventListener('keydown', function(event) {
     if ((event.ctrlKey || event.metaKey) && event.key === 'u') {
