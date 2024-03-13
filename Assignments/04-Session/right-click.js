fe9721b3595e482d0a93ffd8f199ad93136c42c8
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2024-03-13 06:14:45 +0530

    Fix the code

--- Assignments/04-Session/right-click.js
+++ Assignments/04-Session/right-click.js
@@ -17,7 +17,7 @@ document.addEventListener('contextmenu', function(event) {
 document.addEventListener('keydown', function(event) {
     if ((event.ctrlKey || event.metaKey) && (event.key === 'u' || (event.shiftKey && event.code === 'KeyI'))) {
         event.preventDefault();
-        alert('Viewing page source is disabled.');
+        alert('Viewing page source is disabled 😜.');
     }
 });
 
