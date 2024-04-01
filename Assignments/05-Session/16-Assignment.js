cbe2672eddd2afd0b5af69ee458b1118dfd10a1c
vigneshvaranasi <varanasivsv@gmail.com>
2024-04-01 20:49:38 +0530

    fix typo

--- Assignments/05-Session/16-Assignment.js
+++ Assignments/05-Session/16-Assignment.js
@@ -4,8 +4,8 @@ let b=10;
 console.log("The value of a is: "+a);
 console.log("The value of b is: "+b);
 
-console.log("\nArithmetic operations\n")
-;console.log("The Addition of a and b is: "+(a+b));
+console.log("\nArithmetic operations\n");
+console.log("The Addition of a and b is: "+(a+b));
 console.log("The Subtraction of a and b is: "+(a-b));
 console.log("The Multiplication of a and b is: "+(a*b));
 console.log("The Division of a and b is: "+(a/b));
