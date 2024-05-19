c814a5f055b2bf608365f2362ea22d2cd344bd34
vigneshvaranasi <varanasivsv@gmail.com>
2024-05-19 22:31:44 +0530

    intro to GSAP

---
+++ GSAP/Introduction/script.js
@@ -0,0 +1,11 @@
+gsap.to("#box1",{
+    x: 300,
+    duration: 2,
+    //delay in seconds start action after 5 seconds
+    // delay: 5,
+})
+gsap.to("#box2",{
+    x: 500,
+    y: 200,
+    duration: 2,
+})
