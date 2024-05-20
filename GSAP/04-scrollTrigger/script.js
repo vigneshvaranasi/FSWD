a133ade43ffe266bca66f85ad281acae53f4f8eb
vigneshvaranasi <varanasivsv@gmail.com>
2024-05-20 15:41:17 +0530

    add gsap timeline & scroll trigger

---
+++ GSAP/04-scrollTrigger/script.js
@@ -0,0 +1,63 @@
+gsap.from("#page1 .mainhead",{
+    opacity:0,
+    delay:1,
+    duration:2,
+    y:-30,
+})
+gsap.from("#page2 h2",{
+    opacity:0,
+    duration:2,
+    x: 500,
+    scrollTrigger:{
+        trigger:"#page2 h2",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+    }
+})
+gsap.from("#page2 h3",{
+    opacity:0,
+    duration:2,
+    x: -500,
+    scrollTrigger:{
+        trigger:"#page2 h3",
+        scroller:"body",
+        // markers:true,
+        start:"top 60%",
+    }
+})
+gsap.from("#page3 h2",{
+    opacity:0,
+    duration:2,
+    x: 200,
+    scrollTrigger:{
+        trigger:"#page3 h2",
+        scroller:"body",
+        markers:true,
+        start:"top 90%",
+        scrub:true,
+    }
+})
+gsap.from("#page3 h3",{
+    opacity:0,
+    duration:2,
+    x: -200,
+    scrollTrigger:{
+        trigger:"#page3 h3",
+        scroller:"body",
+        markers:true,
+        start:"top 90%",
+        scrub:true,
+    }
+})
+gsap.from("#page4 h2",{
+    opacity:0,
+    duration:2,
+    x: -600,
+    scrollTrigger:{
+        trigger:"#page4 h2",
+        scroller:"body",
+        // markers:true,
+        start:"top 80%",
+    }
+})
--- GSAP/README.md
+++ GSAP/README.md
@@ -1,4 +1,5 @@
 # GSAP
 1. [Introduction - gsap.to()](https://vigneshvaranasi.github.io/FSWD/GSAP/01-Introduction/)
 2. [gsap.from()](https://vigneshvaranasi.github.io/FSWD/GSAP/02-gsap.from()/)
-2. [GASP Timeline](https://vigneshvaranasi.github.io/FSWD/GSAP/03-gsap.timeline()/)
+3. [GASP Timeline](https://vigneshvaranasi.github.io/FSWD/GSAP/03-gsap.timeline()/)
+3. [Scroll Trigger](https://vigneshvaranasi.github.io/FSWD/GSAP/04-scrollTrigger()/)
