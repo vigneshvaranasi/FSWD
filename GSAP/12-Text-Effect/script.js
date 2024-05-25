b33b07a1ef5ee87698d06bef9b4868081e769cd9
vigneshvaranasi <varanasivsv@gmail.com>
2024-05-25 15:04:33 +0530

    Add textfx into animation code

--- GSAP/12-Text-Effect/script.js
+++ GSAP/12-Text-Effect/script.js
@@ -6,7 +6,10 @@ tl.from('#page1 h1',{
     delay: 0.5,
     ease: "expoScale(0.5,7,none)",
 })
-tl.to('#page1 h1 .letter-a',{
+
+let tl2 = gsap.timeline({delay:1.5})
+let tl3 = gsap.timeline({delay:1.5})
+tl2.to('#page1 h1 .letter-a',{
     duration: 1,
     y: 200,
     opacity: 0,
@@ -15,18 +18,27 @@ tl.to('#page1 h1 .letter-a',{
     ease: "slow(0.7,0.7,false)",
     text: "",
 })
+tl3.to('#page1 h1 .space1',{
+    duration: 0.1,
+    text:"&#10240;",
+    delay: 0.1,
+})
+tl3.to('#page1 h1 .space1',{
+    duration: 0.3,
+    text:"",
+    delay: 0.2,
+})
 tl.to('#page1 h1 .letter-e',{
     duration: 1,
     y: 200,
     opacity: 0,
-    delay: 0.1,
+    delay: 0.2,
     ease: "slow(0.7,0.7,false)",
     position: "absolute",
     text: "",
 })
-tl.to('#page1 h1 .dash',{
-    duration: 0.5,
+tl3.to('#page1 h1 .dash',{
+    duration: 0.2,
     text:" - ",
-    y: 200,
     delay: 0.2,
 })
