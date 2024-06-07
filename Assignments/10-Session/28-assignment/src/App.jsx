09b6fb2e512286f3d78b8b977f4dc97fbc7e66b8
vigneshvaranasi <varanasivsv@gmail.com>
2024-06-07 19:13:12 +0530

    Finish Assignment 28

---
+++ Assignments/10-Session/28-assignment/src/App.jsx
@@ -0,0 +1,67 @@
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+function App() {
+
+  const { register, handleSubmit } = useForm();
+  let [users, setUsers] = useState([]);
+
+  // From Submission
+  function handleFormSubmit(userObj) {
+    console.log('userObj: ', userObj);
+    setUsers([...users, userObj]);
+  }
+  return (
+    <div>
+      <h1 className="text-center mt-4">Assignment 28</h1>
+      <h2 className='mt-5 text-center'>User Register</h2>
+      <form className="w-50 mx-auto mt-3" onSubmit={handleSubmit(handleFormSubmit)}>
+        {/* Username */}
+        <div className="mt-2">
+          <label htmlFor="username" className="form-lable">Username</label>
+          <input type="text" {...register('username')} id="username" className="form-control" />
+        </div>
+        {/* DOB */}
+        <div className="mt-2">
+          <label htmlFor="dob" className="form-lable">Date Of Birth</label>
+          <input type="date" {...register('dob')} id="dob" className="form-control" />
+        </div>
+        {/* City */}
+        <div className="mt-2">
+          <label htmlFor="city" className="form-lable">City</label>
+          <input type="text" {...register('city')} id="city" className="form-control" />
+        </div>
+        {/* Submit */}
+        <div>
+          <button type="submit" className="btn btn-primary mt-3">Register</button>
+        </div>
+      </form>
+      <h2 className=' mt-5 text-center'>User List</h2>
+
+      <div className='m-4'>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Date Of Birth</th>
+              <th>City</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map((user, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{user.username}</td>
+                    <td>{user.dob}</td>
+                    <td>{user.city}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+export default App;
