28575f223186c30bc9db36b1ec7dbf7719d7fa7f
vigneshvaranasi <varanasivsv@gmail.com>
2024-06-08 09:43:30 +0530

    Add form validation

--- 05-React/10-forms/src/Register.jsx
+++ 05-React/10-forms/src/Register.jsx
@@ -4,7 +4,7 @@ function Register(){
 
 
     // const {register, handleSubmit,formState:{errors}} = useForm();
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit,formState: { errors }} = useForm();
 
     // From Submission
     function handleFormSubmit(userObj){
@@ -19,17 +19,22 @@ function Register(){
             {/* Username */}
             <div className="mt-2">
                 <label htmlFor="username" className="form-lable">Username</label>
-                <input type="text" {...register('username')} id="username" className="form-control"/>
+                <input type="text" {...register('username',{required:true,maxLength:8,minLength:4})} id="username" className="form-control"/>
+                {errors.username?.type==='required' && <p className="text-danger">Username is required</p>}
+                {errors.username?.type==='minLength' && <p className="text-danger">Min Lenght Should be 4</p>}
+                {errors.username?.type==='maxLength' && <p className="text-danger">Max Lenght Should be 8</p>}
             </div>
             {/* Email */}
             <div className="mt-3">
                 <label htmlFor="email" className="form-lable">Email</label>
-                <input type="email" id="email" {...register('email')} className="form-control"/>
+                <input type="email" id="email" {...register('email',{required:true})} className="form-control"/>
+                {errors.email?.type==='required' && <p className="text-danger">Email is required</p>}
             </div>
             {/* Password */}
             <div className="mt-3">
                 <label htmlFor="password" className="form-lable">Password</label>
-                <input type="password" id="password" {...register('password')} className="form-control"/>
+                <input type="password" id="password" {...register('password',{required:true})} className="form-control"/>
+                {errors.password?.type==='required' && <p className="text-danger">Password is required</p>}
             </div>
             {/* Submit */}
             <div>
