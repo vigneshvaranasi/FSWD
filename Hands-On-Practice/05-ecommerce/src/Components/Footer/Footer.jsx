ca7696a7e345394f85b5b03f7301cb88a99ef51b
vigneshvaranasi <varanasivsv@gmail.com>
2024-06-15 19:40:49 +0530

    Add about

--- Hands-On-Practice/05-ecommerce/src/Components/Footer/Footer.jsx
+++ Hands-On-Practice/05-ecommerce/src/Components/Footer/Footer.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import './Footer.css'
-import amzlogo from '../../assets/amazon.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Footer.css';
+import amzlogo from '../../assets/amazon.png';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div>
-      <footer className="footer text-white">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <img src={amzlogo} width={100} className='mt-3 mb-3' alt="" />
-              <p>Amazon is the largest online retailer in the world. It is an e-commerce and cloud computing company.</p>
-            </div>
-            <div className="col-md-4">
-              <h3>Links</h3>
-              <Link to={""}>Home</Link>
-              <Link to={"register"}>Register</Link>
-              <Link to={"login"}>Login</Link>
-              <Link to={"about"}>About</Link>
-            </div>
-            <div className="col-md-4">
-              <h3>Contact Us</h3>
-              <p>123, Main Street, New York, USA</p>
-              <p>
-                <a href="tel:+1234567890">+1 234 567 890</a>
-              </p>
-            </div>
+    <footer className="footer text-white py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src={amzlogo} width={100} className="mb-3" alt="Amazon Logo" />
+            <p>Amazon is the largest online retailer in the world. It is an e-commerce and cloud computing company.</p>
+          </div>
+          <div className="col-md-4">
+            <h3>Links</h3>
+            <ul className="list-unstyled">
+              <li><Link to="/" className="text-white">Home</Link></li>
+              <li><Link to="/register" className="text-white">Register</Link></li>
+              <li><Link to="/login" className="text-white">Login</Link></li>
+              <li><Link to="/about" className="text-white">About</Link></li>
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <h3>Contact Us</h3>
+            <p>123 Main Street, New York, USA</p>
+            <p>
+              <a href="tel:+1234567890" className="text-white">+1 234 567 890</a>
+            </p>
           </div>
         </div>
-      </footer>
-    </div>
-  )
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
