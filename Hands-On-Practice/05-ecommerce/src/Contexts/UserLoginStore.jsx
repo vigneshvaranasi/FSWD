d983ad3efd579da1f2352f40dd06d2f46be9e918
vigneshvaranasi <varanasivsv@gmail.com>
2024-06-16 20:13:29 +0530

    fix login Password issue

--- Hands-On-Practice/05-ecommerce/src/Contexts/UserLoginStore.jsx
+++ Hands-On-Practice/05-ecommerce/src/Contexts/UserLoginStore.jsx
@@ -4,19 +4,41 @@ import { userLoginContext } from './userLoginContext';
 function UserLoginStore({ children }) {
     const [currentUser, setCurrentUser] = useState(null);
     const [userLoginStatus, setUserLoginStatus] = useState(false);
-
+    
     async function handleLogin(userObj) {
         let response = await fetch(`http://localhost:4000/users?username=${userObj.username}&password=${userObj.password}`);
         let usersData = await response.json();
         console.log('usersData: ', usersData);
-        if (usersData.length === 0) {
+
+        // If the Username is not found in the show alert
+        if(usersData.length === 0){
+            alert('Invalid Credentials');
             // Invalid Credntials
             setCurrentUser(null);
             setUserLoginStatus(false)
-        } else {
-            setCurrentUser(usersData[0]);
-            setUserLoginStatus(true)
         }
+        // If the username is there and the password is wrong
+        else if(usersData[0].password !== userObj.password){
+            // Invalid Credntials
+            setCurrentUser(null);
+            setUserLoginStatus(false)
+            alert('Wrong Password');
+        }
+        // If the username and password are correct
+        else{
+            setCurrentUser(usersData[0]);
+            setUserLoginStatus(true);
+        } 
+
+        
+        // if (usersData.length === 0) {
+        // } else {
+        //     setCurrentUser(usersData[0]);
+        //     setUserLoginStatus(true)
+        //     console.log('userLoginStatus: ', userLoginStatus);
+        //     console.log('currentUser: ', currentUser);
+            
+        // }
     }
 
     // User Logout
