ed23a51da74258f600d04e7b609dc43a86ff0099
vigneshvaranasi <varanasivsv@gmail.com>
2024-07-27 11:53:46 +0530

    chore: Update .gitignore and VSCode settings

--- Hands-On-Practice/06-ecommerce - FullStack/Backend/server.js
+++ Hands-On-Practice/06-ecommerce - FullStack/Backend/server.js
@@ -46,6 +46,7 @@ mc.connect().then(connectionObject => {
     // Assign Port Number to Server
     const port = process.env.PORT || 4000;
     app.listen(port, () => {
+        
         console.log(`Server running on port http://localhost:${port}`);
     });
 }).catch(err => {
