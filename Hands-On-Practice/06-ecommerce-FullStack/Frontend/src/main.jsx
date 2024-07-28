d05291bb26959c3af85cc597be9b14bf87ba8b52
vigneshvaranasi <varanasivsv@gmail.com>
2024-07-28 22:23:04 +0530

    chore: Include external CSS file for ReactToastify in vite.config.js

--- Hands-On-Practice/06-ecommerce-FullStack/Frontend/src/main.jsx
+++ Hands-On-Practice/06-ecommerce-FullStack/Frontend/src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UserLoginStore from './Contexts/UserLoginStore.jsx';
-import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
