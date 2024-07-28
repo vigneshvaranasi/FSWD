3f860c78895e61ae16f564c965c1c0eff72d2ff9
vigneshvaranasi <varanasivsv@gmail.com>
2024-07-28 21:46:14 +0530

    chore: Update vite.config.js to include external CSS file for ReactToastify

--- Hands-On-Practice/06-ecommerce-FullStack/Frontend/src/main.jsx
+++ Hands-On-Practice/06-ecommerce-FullStack/Frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UserLoginStore from './Contexts/UserLoginStore.jsx';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- Hands-On-Practice/06-ecommerce-FullStack/Frontend/vite.config.js
+++ Hands-On-Practice/06-ecommerce-FullStack/Frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-toastify/dist/ReactToastify.css']
-    }
-  }
+  plugins: [react()]
 })
