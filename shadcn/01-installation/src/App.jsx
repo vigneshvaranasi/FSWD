34cfe33872c381577426fcf5176da143cc6e8ee2
vigneshvaranasi <varanasivsv@gmail.com>
2024-09-01 14:20:20 +0530

    add shadcn

---
+++ shadcn/01-installation/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+function App() {
+  return (
+    <div className='text-center'>
+      <h1 className='text-4xl mt-10 text-blue-400'>Hello</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
