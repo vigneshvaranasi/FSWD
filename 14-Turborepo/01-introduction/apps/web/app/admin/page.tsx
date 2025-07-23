ee6fae861c52e97561f5bd0ab849d17d43c316a4
vigneshvaranasi <varanasivsv@gmail.com>
2025-07-23 06:27:53 +0530

    finish turbo repo intro

---
+++ 14-Turborepo/01-introduction/apps/web/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import Admin from "@repo/ui/Admin"
+
+function Page() {
+  return (
+    <div>
+        <Admin />
+    </div>
+  )
+}
+
+export default Page
---
+++ 14-Turborepo/01-introduction/packages/ui/src/Admin.tsx
@@ -0,0 +1,8 @@
+
+function Admin() {
+  return (
+    <div><h1>Admin Component</h1></div>
+  )
+}
+
+export default Admin
