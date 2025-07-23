e5035937b0bddd519477bfd8568d7db1e12d2127
vigneshvaranasi <varanasivsv@gmail.com>
2025-07-23 06:53:18 +0530

    add react to turbo repo

---
+++ 14-Turborepo/02-react/apps/react-app/src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@repo/ui/button'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        Hello Vite + React!
+      </h1>
+      <Button appName='react-app'>
+        Hello Button form react
+      </Button>
+    </>
+  )
+}
+
+export default App
